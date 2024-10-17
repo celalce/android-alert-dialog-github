19cdd6fb9a23520e0f15bee8e1c71d6375b14664
celal albayrak <celalce25@hotmail.com>
2024-10-17 13:05:01 +0300

    alertDialog edit

--- app/src/main/java/com/celalalbayrak/alertdialog/MainActivity.kt
+++ app/src/main/java/com/celalalbayrak/alertdialog/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.celalalbayrak.alertdialog
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.celalalbayrak.alertdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //baslarken email ve parolımızı tutmak için iki değişken oluşturalmı
+
+        val girisemal = ""
+        val girisparola= ""
+
+        binding.tvuyeol.setOnClickListener {
+            var activity_uyeol = LayoutInflater.from(this).inflate(R.layout.activity_uyeol, null)
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setView(activity_uyeol)
+            alertDialog.setNegativeButton("Kapat"){dialog, which->}
+            alertDialog.show()
+        }
+
+
+
+
     }
 }
